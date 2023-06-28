f0a80586171aa7829d758d073d5306da6716b63e
WithMyGitHub Pemrograman <136155357+m-adin-informatika@users.noreply.github.com>
2023-06-28 19:27:00 +0700

    Update latihan2.bat

--- latihan2.bat
+++ latihan2.bat
@@ -1,1 +1,15 @@
-Latihan 2
+@echo off
+title Program Operasi Hitung
+
+rem Masukan Variabel
+set /p a=Masukkan Nilai a :
+set /p b=Masukkan Nilai b :
+set /p c=Masukkan Nilai c :
+
+rem Menghitung (a*b)^2+(b-c)^2/(a-c)
+set /a hitung=((a*b)*(a*b)+(b-c)*(b-c))/(a-c)
+
+echo HASIL
+echo Hasil (%a%*%b%)**2+(%b%-%c%)**2/(%a%-%c%):%hitung%
+echo
+pause
