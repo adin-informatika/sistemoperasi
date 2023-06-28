4c9b6f27f88ca9473d62270c88710629f396a81d
WithMyGitHub Pemrograman <136155357+m-adin-informatika@users.noreply.github.com>
2023-06-28 19:22:59 +0700

    Update latihan1.bat

--- latihan1.bat
+++ latihan1.bat
@@ -1,1 +1,31 @@
-Latihan 1
+@echo off
+title Latihan 5 Perhitungan Luas Segitiga
+rem ======================Setting Variabel ====================
+echo------------------------------------------------------------
+echo INPUTAN NILAI ALAS DAN TINGGI
+echo------------------------------------------------------------
+set/p alas= masukkan nilai alas :
+set/p tinggi= masukkan nilai tinggi :
+
+rem ======================Setting Perhitungan =================
+set/a luas = alas*tinggi / 2
+
+if %luas% GEQ 500 {
+set grade = Kategori Besar
+} else if %luas% GEQ 100{
+set grade =Kategori Sedang
+} else {
+set grade =Kategori Kecil
+}
+
+cls
+rem =====================Output===========================
+echo------------------------------------------------------
+echo Perhitungan Luas Segitiga
+echo-------------------------------------------------------
+echo alas : %alas%
+echo tinggi:%tinggi%
+echo-------------------------------------------------------
+echo luas segitiga tersebut adalah : % Luas %
+echo kategori bangunannya adalah:% grade %
+pause
