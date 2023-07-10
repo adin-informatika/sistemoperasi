e8eb765fd7b58a350ed7c6f1e3f1fa2fb33a465a
WithMyGitHub Pemrograman <136155357+m-adin-informatika@users.noreply.github.com>
2023-07-10 21:33:59 +0700

    Update latihan_inputlogin.bat

--- latihan_inputlogin.bat
+++ latihan_inputlogin.bat
@@ -1,1 +1,18 @@
-Membuat input login
+@echo off
+color 7
+
+:login
+cls 
+echo ***********************************************
+echo                 LOGIN USER         
+echo ***********************************************
+echo.
+set /p name=      User     :
+set /p password=  Password :
+if %password% ==  password (
+	goto begin 
+) else (
+	echo Password salah 
+	set /p x= enter......!!!
+	goto login 
+)
