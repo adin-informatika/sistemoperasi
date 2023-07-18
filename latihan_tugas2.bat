027e23466525a5fcf5b2d6d94cf0258497d0646a
WithMyGitHub Pemrograman <136155357+m-adin-informatika@users.noreply.github.com>
2023-07-18 07:28:07 +0700

    Update latihan_tugas2.bat

--- latihan_tugas2.bat
+++ latihan_tugas2.bat
@@ -1,1 +1,21 @@
-Membuat Latihan Tugas 2
+@echo off
+title Menghitung Operasi Matematika 2
+echo -------------- Menghitung Variabel --------------
+echo a = 5
+echo b = 7
+echo c = 6
+echo d = 110
+echo e = 50
+echo f = 4
+echo Menghitung nilai ((5+5)(7*6))/((110-50)-(4*5))
+rem ============== SETTING NILAI DAN VARIABEL ==============
+set /a a= 5
+set /a b= 7
+set /a c= 6
+set /a d= 110
+set /a e= 50
+set /a f= 4
+set /a hasil = ((5+5)(7*6))/((110-50)-(4*5))
+echo -------------- Hasil Perhitungan --------------
+echo Hasilnya adalah : %hasil%
+pause
